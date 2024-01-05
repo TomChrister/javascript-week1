2dc241fda7776b02af045d9a7289f0dbf0d88656
TomChrister <142898062+TomChrister@users.noreply.github.com>
2024-01-05 13:40:02 +0100

    no message

---
+++ loops/script.js
@@ -0,0 +1,52 @@
+/*// Loops is for repeating
+
+/!*for (initialization; condition; increment) {
+
+}*!/
+
+for (let i = 0; i <= 5; i++){
+    console.log(i);
+}
+
+// i = 0
+// true
+// i = 1
+
+
+// i = 1
+// true
+// console.log (1)
+// 1 + 1 = 2
+
+for (let i = 1; i <= 10; i++){
+    console.log(i);
+}*/
+
+              // 0        1      2         3
+const cars = ["Tesla", "BMW", "Volvo", "Bugatti"];
+
+console.log(cars [1]);
+console.log(cars [2]);
+console.log(cars [length])
+
+console.log(cars.length);
+
+const totalNumberOfCars = cars.length;
+
+for (let i = 0; i <= totalNumberOfCars; i++) {
+}
+
+const fruits = ["Apple", "Banana", "Cherry"];
+
+const totalNumberOfFruits = fruits.length;
+
+for (let x = 0; x <=totalNumberOfFruits; x++){
+}
+
+// How to get the last item in the array
+console.log("Total number of arrays", fruits.length)
+console.log("Fruit length minus 1", fruits.length - 1);
+
+const  lastItemInTheArray = fruits.length - 1;
+console.log(fruits[lastItemInTheArray]);
+
