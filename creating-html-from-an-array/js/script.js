5a294a88d9d4fffe4621e8a4ea7ae39fe461fef3
TomChrister <142898062+TomChrister@users.noreply.github.com>
2024-01-05 14:09:16 +0100

    no message

--- creating-html-from-an-array/js/script.js
+++ creating-html-from-an-array/js/script.js
@@ -18,7 +18,25 @@ const pets = [
     },
 ];
 // Select the pet-container class
+const petContainer = document.querySelector(".pet-container");
+console.log("petContainer", petContainer)
 
 // Create a html variable to hold our html in it when we use the loop
+let html = "";
+
 // Loop over the array
-// change the innerHTML
+const totalItemsOfPet = pets.length;
+console.log(totalItemsOfPet);
+
+for (let i = 0; i < totalItemsOfPet; i++) {
+    console.log(pets[i].colour);
+    console.log(pets[i].age)
+
+    html += `
+                <div><p>Color: ${pets[i].colour}</p></div>
+    `;
+}
+
+
+
+// change the innerHTML of the pet container
