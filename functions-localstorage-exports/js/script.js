62390713ce0d696c236f583cb544566f563f6965
TomChrister <142898062+TomChrister@users.noreply.github.com>
2024-01-10 14:46:25 +0100

    no message

---
+++ functions-localstorage-exports/js/script.js
@@ -0,0 +1,20 @@
+// variables
+
+// Ecmascript
+
+for (let i = 1; i < 11; i++) {
+    console.log(1);
+}
+
+let myBigBox = ["hesham", 22232, false, {name: "tom"}];
+
+console.log(myBigBox.length);
+
+console.log(myBigBox);
+
+console.log(myBigBox[0]);
+
+console.log(myBigBox[3].name);
+
+const lastItemIndex = myBigBox.length - 1;
+console.log(lastItemIndex);
