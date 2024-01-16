daf0856b7bb89f2715b29508a46f2fa79c2ecce1
TOMCHRISTER\tomcs <tomchrister.s@outlook.com>
2024-01-16 14:08:58 +0100

    end of the commit

---
+++ building-a-favourite-page/js/data/products.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        id:1,
+        name: "iPhone 15",
+        price: 10000
+    },
+    {
+        id:2,
+        name: "iPhone 14",
+        price: 90000
+    },
+    {
+        id:3,
+        name: "iPhone 13",
+        price: 80000
+    },
+    {
+        id:4,
+        name: "iPhone 12",
+        price: 70000
+    }
+];
