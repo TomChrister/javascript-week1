07c1d293adc3ba60bd5dd75aea0b64c665f43211
TOMCHRISTER\tomcs <tomchrister.s@outlook.com>
2024-01-26 12:51:25 +0100

    no message

---
+++ arrow-functions/js/script.js
@@ -0,0 +1,26 @@
+// Variable function
+
+const showName = function (name){
+    console.log("The name is: " + name);
+}
+
+showName("hesh");
+
+
+
+// Fat arrow functions
+
+const showPetName = (name) => {
+    console.log("The name is: " + name);
+}
+
+showPetName("Tom");
+
+
+
+
+const sum = (num1, num2, num3, num4) => {
+    console.log(num1 + num2 + num3 + num4);
+}
+
+sum (100, 200, 3, 6);
