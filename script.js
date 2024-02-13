7c5e8416da0f89fc79bcf476bceca4bbdea49344
TOMCHRISTER\tomcs <tomchrister.s@outlook.com>
2024-02-13 13:29:16 +0100

    no message

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 // variables //
 
 var myName = "Tom";
@@ -86,11 +87,11 @@ console.log(convertedYear);
 
 // Convert from number to string
 
-/*let num = 123;
+/!*let num = 123;
 console.log(num.toString())
 
 let convertedNumber = num.toString();
-console.log(convertedNumber)*/
+console.log(convertedNumber)*!/
 
 var city = "Oslo";
 console.log(city)
@@ -130,4 +131,75 @@ let convertedNumber2 = num2.toString();
 console.log(convertedNumber2)
 
 let numString = num + num2;
-console.log(numString.toString())
+console.log(numString.toString())*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Example array
+let originalArray = ["Book1", "Book2", "Book3", "Book4", "Book5", "Book6"];
+
+// Function to split array into chunks
+function splitArray(array, chunkSize) {
+    let result = [];
+    for (let i = 0; i < array.length; i += chunkSize) {
+        result.push(array.slice(i, i + chunkSize));
+    }
+    return result;
+}
+
+// Split the original array into chunks of size 2
+let splitArrays = splitArray(originalArray, 2);
+
+console.log(splitArrays);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const games = ["MW1", "MW2", "MW3", "MW4", "MW5", "MW6"];
+
+function splitGames (games, chunks) {
+    let result = [];
+    for (let i = 0; i < games.length; i += chunks) {
+        result.push(games.slice(i, i, chunks));
+    }
+    return result;
+}
+
+let splittedGames = splitGames(games, 2);
+console.log(splittedGames);
